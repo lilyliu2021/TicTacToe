62df5ace8655cc47ebc73012fb36f63344bb5264
lilyliu2021 <lily.liu201209@gmail.com>
2023-03-30 23:16:09 -0700

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlayAgain = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(229, 140);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(552, 309);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(375, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click Start Button To Start Game";
+            // 
+            // PlayAgain
+            // 
+            this.PlayAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayAgain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.PlayAgain.Location = new System.Drawing.Point(437, 464);
+            this.PlayAgain.Name = "PlayAgain";
+            this.PlayAgain.Size = new System.Drawing.Size(135, 35);
+            this.PlayAgain.TabIndex = 4;
+            this.PlayAgain.Text = "Play Again";
+            this.PlayAgain.UseVisualStyleBackColor = true;
+            this.PlayAgain.Click += new System.EventHandler(this.PlayAgain_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Y";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(54, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Select Axis";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Start.Location = new System.Drawing.Point(229, 464);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(135, 35);
+            this.Start.TabIndex = 10;
+            this.Start.Text = "Start Game";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(290, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 38);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(668, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 38);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "O";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(291, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(492, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 32);
+            this.label7.TabIndex = 14;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(675, 64);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 32);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "0";
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Exit.Location = new System.Drawing.Point(646, 464);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(135, 35);
+            this.Exit.TabIndex = 16;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 574);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PlayAgain);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PlayAgain;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] boardArray = new string[3, 3];
+        string turn = "";
+        int gameStep = 0;        
+        bool isWin=false;
+        int xWinCount = 0;
+        int oWinCount = 0;
+        string MessageBoxTitle = "Continue/Exit";
+        string MessageBoxContent = "Want to Play Again?";
+        void initial()
+        {
+            xWinCount = 0;
+            oWinCount = 0;
+            label6.Text = xWinCount.ToString();
+            label8.Text = oWinCount.ToString();
+            button4.Text = "Select Axis";
+            resetGame();           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            turn = "";
+            initial();
+        }
+        void PrintBoard(string toFill = "")
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            richTextBox1.Clear();
+            for (int i = 0; i <= boardArray.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= boardArray.GetUpperBound(1); j++)
+                {
+                    if (toFill == " - ")
+                    {
+                        boardArray[i, j] = toFill;
+                    }
+                    richTextBox1.Text += boardArray[i,j];
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+        string randomTurn()
+        {
+            Random randomObj = new Random();
+            int randomNum = randomObj.Next(2);
+            if (randomNum == 0)
+            {
+                turn = " X ";
+            }
+            else
+            {
+                turn = " O ";
+            }
+            return turn;
+        }
+
+        void clickEvent(int i, int j)
+        {
+            if (checkValidMove(i, j))
+            {             
+                CheckTie(gameStep);
+                //gameStep++;
+                if ((turn == " X ") && gameStep <= 8)
+                {
+                    boardArray[i, j] = " X ";
+                    gameStep++;
+                    PrintBoard(" X ");                    
+                    if (winCheck(turn) == "")
+                    {
+                        turn = " O ";
+                        button4.Text = turn + " turn";
+                    }
+                    return;
+                }
+                else if ((turn == " O ") && gameStep <= 8)
+                {
+                    boardArray[i, j] = " O ";
+                    PrintBoard(" O ");
+                    gameStep++;
+                    if (winCheck(turn) == "")
+                    {
+                        turn = " X ";
+                        button4.Text = turn + " turn";
+                    }
+                    return;
+                }                 
+            }
+        }
+       
+        bool checkValidMove(int i, int j)
+        {
+            if (boardArray[i,j] == " X " || boardArray[i, j] ==" O ")
+                {
+                MessageBox.Show("Please make a valid move!");
+                return false;               
+                }
+                else
+                {
+                  return true;
+                }
+        }
+
+        void winEvent(string marker)
+        {
+            label1.Text = $"{marker} win!!! ,Game Over";
+            if (marker==" X ")
+            {
+                xWinCount++;
+                isWin = true;
+            }
+            else
+            {
+                oWinCount++;
+                isWin = true;
+            }
+            label6.Text =xWinCount .ToString();
+            label8.Text =oWinCount .ToString();
+            
+            //gameStep = 0;
+            DialogResult dialogResult = MessageBox.Show(MessageBoxContent, MessageBoxTitle, MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {               
+                resetGame();
+                label1.Text = "Click Start Button To Start Game";
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                initial();               
+            }
+        }
+        string winCheck(string marker)
+        {
+            // Check horizontal rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (boardArray[i, 0] == marker && boardArray[i, 1] == marker && boardArray[i, 2] == marker)
+                {                    
+                    winEvent(marker); 
+                }
+            }
+            // Check vertical columns
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardArray[0, j] == marker && boardArray[1, j] == marker && boardArray[2, j] == marker)
+                {                    
+                    winEvent(marker);
+                }
+            }
+
+            // Check diagonal from top-left to bottom-right
+            if (boardArray[0, 0] == marker && boardArray[1, 1] == marker && boardArray[2, 2] == marker)
+            {                
+                winEvent(marker);
+            }
+
+            // Check diagonal from top-right to bottom-left
+            if (boardArray[0, 2] == marker && boardArray[1, 1] == marker && boardArray[2, 0] == marker)
+            {               
+                winEvent(marker);
+            }
+            return "";
+        }
+        void CheckTie(int stepCount)
+        {
+            // Check if board is all filled and no player won
+            if ((stepCount == 9) && !(isWin))
+            {
+                label1.Text = "Draw!!!";
+                DialogResult dialogResult = MessageBox.Show("Play Again?", "Restart", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    resetGame();
+                    label1.Text = "Click Start Button To Start Game";
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    initial();
+                }
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        void resetGame()
+        {
+            string[,] boardArray = new string[3, 3];
+            richTextBox1.Clear();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            PrintBoard(" - ");
+            gameStep = 0;           
+            textBox1.Clear();
+            textBox2.Clear(); 
+            isWin = false;
+            turn = "";
+            button4.Text = "Select Axis";
+            label1.Text = "Click Start Button To Start Game";
+        }        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Please input data ");
+                return;
+            }
+            int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            if (y == 1)
+            {
+                clickEvent(y+1,x-1); return; 
+            }
+            else if (y == 2)
+            {
+                clickEvent (y-1,x-1); return;
+            }
+            else if (y == 3)
+            {
+                clickEvent (y-3,x-1); return;
+            }
+            else{
+                MessageBox.Show("Please input data from 1 to 3");
+            }
+        }
+
+        private void PlayAgain_Click(object sender, EventArgs e)
+        {
+            resetGame();
+            turn = randomTurn();
+            button4.Text = turn + " turn";
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            initial();
+            turn = randomTurn();
+            button4.Text = turn + " turn";
+        }       
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            initial();            
+        }
+    }
+}
+    
+
